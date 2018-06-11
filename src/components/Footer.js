cebb24864a6572730768b07cb50da97cd20e0cdd
Christopher Nemeth <cnemeth1@gmail.com>
2018-06-11 11:06:15 -0600

    Added React App Files + header and footer components.

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => (
+  <h4>Copyright 2018</h4>
+)
+
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Router from './components/Router';
+import './css/styles.css';
+
+render(<Router />, document.querySelector('#main'));
