de98415571940be8ee5326884b0731a2cd99fc2d
Christopher Nemeth <cnemeth1@gmail.com>
2018-06-12 17:32:03 -0600

    Updated deals cards to one row and 3 columns

--- src/components/Deals.js
+++ src/components/Deals.js
@@ -1,28 +1,52 @@
 import React from 'react';
 import Map from './Map';
+import { Grid, Card, Button } from 'semantic-ui-react';
+
 
 class Deals extends React.Component {
     render() {
-
         console.table(this.props.details);
-
-        return(
-            <div className="deal-card">
-                <div>
-                    <p>Restaurant: {this.props.details.restaurant}</p>
-                    <p>Phone: {this.props.details.phone}</p>
-                    <p>Website: {this.props.details.site}</p>
-                </div>
-                <div className="map-holder">
-                    <Map className="map" latlon={this.props.details.location}/>
-                    <div>
-                        <button>Delete</button>
-                        <button>Submit</button>
-                    </div>
-                </div>
+        return (
+            <div>
+                <Card.Group centered={true}>
+                <Card>
+                    <Card.Content>
+                        <Grid>
+                            <Grid.Row>
+                                <Grid.Column width={10}>
+                                    <Card.Description content={`Restaurant: ${this.props.details.restaurant}`}>
+                                    </Card.Description>
+                                    <Card.Description content={`Phone: ${this.props.details.phone}`}>
+                                    </Card.Description>
+                                    <Card.Description content={`Website: ${this.props.details.site}`}>
+                                    </Card.Description>
+                                    <Card.Description content={`Days Open: ${this.props.details.day}`}>
+                                    </Card.Description>
+                                    <Card.Description content={`Hours: ${this.props.details.time}`}>
+                                    </Card.Description>
+                                </Grid.Column>
+                                <Grid.Column width={30}>
+                                    <Card.Description content={`Today's Specials: ${this.props.details.special}`}>
+                                    </Card.Description>
+                                </Grid.Column>
+                                <Grid.Column width={40}>
+                                    <div className="map-holder">
+                                        <Map className="map" latlon={this.props.details.location}/>
+                                        <div>
+                                            <Button content="Delete" />
+                                            <Button content="Submit" />
+                                        </div>
+                                    </div>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Card.Content>
+                </Card>
+                </Card.Group>
             </div>
         )
     }
 }
 
 export default Deals;
+
