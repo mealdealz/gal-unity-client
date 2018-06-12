09acc01caa682dff2d7a1ba95438d2093af69c08
Elliot Jane Lader <elliot.j.lader@gmail.com>
2018-06-12 12:04:51 -0600

    Added functionality: Maps now center on resturaunt location

--- src/components/ButtonExample.js
+++ src/components/ButtonExample.js
@@ -3,12 +3,7 @@ import { Button } from 'semantic-ui-react'
 
 class ButtonExample extends React.Component {
     render() {
-
-        console.log(this.props.name);
-
         return(
-
-
         <Button className="ui button" >Click Here</Button>
         )
     }
