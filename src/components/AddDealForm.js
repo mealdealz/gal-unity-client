4a47dabdb54a7bfb817fc12b87d91956d8122439
Christopher Nemeth <cnemeth1@gmail.com>
2018-06-13 13:55:44 -0600

    added time picker

--- src/components/AddDealForm.js
+++ src/components/AddDealForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Form, Button, Input, Modal } from 'semantic-ui-react';
+import TimePicker from 'rc-time-picker';
+import 'rc-time-picker/assets/index.css';
+import moment from 'moment';
+import { Form, Button, Input, Checkbox, Modal } from 'semantic-ui-react';
 
 class AddDealForm extends React.Component {
 
@@ -44,6 +47,13 @@ class AddDealForm extends React.Component {
   }
 
   render() {
+    const format = 'h:mm a';
+
+    const now = moment().hour(0).minute(0);
+
+    function onChange(value) {
+      console.log(value && value.format(format));
+    }
     return (
       <div>
         <Modal trigger={<Button>Show Modal</Button>} centered={false}>
@@ -56,9 +66,19 @@ class AddDealForm extends React.Component {
                 <Form.Input placeholder='Website' />
               </Form.Field>
               <Form.Field>
-                <Form.Input placeholder='Address' />
-                <Form.Input placeholder='Phone' />
-                <Form.Checkbox label='I agree to the Terms and Conditions' />
+                <Form.Group>
+                  <Form.Checkbox inline label='M' required />
+                  <Form.Checkbox inline label='T' required />
+                  <Form.Checkbox inline label='W' required />
+                  <Form.Checkbox inline label='Th' required />
+                  <Form.Checkbox inline label='F' required />
+                  <Form.Checkbox inline label='Sat' required />
+                  <Form.Checkbox inline label='Sun' required />
+                </Form.Group>
+                <TimePicker name="Start Time" showSecond={false} defaultValue={now} className="xxx" onChange={onChange} format={format} use12Hours inputReadOnly />
+                <TimePicker name="End Time" showSecond={false} defaultValue={now} className="xxx" onChange={onChange} format={format} use12Hours inputReadOnly />
+                <Form.Input placeholder='Special' />
+                <Form.Input placeholder='' />
                 <Button type='submit'>Submit</Button>
               </Form.Field>
               </Form.Group>
