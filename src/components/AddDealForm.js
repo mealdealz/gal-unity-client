1759bf14ef992f2bba522d7f8a7b031f92695ca8
Christopher Nemeth <cnemeth1@gmail.com>
2018-06-14 13:52:36 -0600

    working on form

--- src/components/AddDealForm.js
+++ src/components/AddDealForm.js
@@ -8,69 +8,146 @@ import { Form, Button, Modal } from 'semantic-ui-react';
 class AddDealForm extends React.Component {
 
   state = {
-    "id": "",
-    "restaurant": "",
-    "site": "",
-    "phone": "",
-    "address": "",
-    "location": [],
-    "specials": [{
-      "special_id": "",
-      "category": "",
-      "days":  [],
-      "start_time": "",
-      "end_time": "",
-      "deals": [{"name": "",
-        "cost": ""},
-        {"name": "",
-          "cost": ""},
-        {"name": "",
-          "cost": ""},
-        {"name":"",
-          "cost":""}]
+    "restaurantName": "",
+    "restaurantSite": "",
+    "restaurantPhone": "",
+    "restaurantAddress": "",
+    "restaurantLocation": [],
+    "restaurantSpecials": [{
+      "specialCategory": "",
+      "specialDays":  [],
+      "startTime": "",
+      "endTime": "",
+      "deals": [{"dealName": "",
+        "dealPrice": ""},
+        {"dealName": "",
+          "dealPrice": ""},
+        {"dealName": "",
+          "dealPrice": ""},
+        {"dealName": "",
+          "dealPrice": ""}]
     }]
   }
 
   resetForm = () => {
     this.setState(
       {
-        "id": "",
-        "restaurant": "",
-        "site": "",
-        "phone": "",
-        "address": "",
-        "location": [],
-        "specials": [{
-          "special_id": "",
-          "category": "",
-          "days":  [],
-          "start_time": "",
-          "end_time": "",
-          "deals": [{"name": "",
-            "cost": ""},
-            {"name": "",
-              "cost": ""},
-            {"name": "",
-              "cost": ""},
-            {"name":"",
-              "cost":""}]
-      }]}
+    "restaurantName": "",
+    "restaurantSite": "",
+    "restaurantPhone": "",
+    "restaurantAddress": "",
+    "restaurantLocation": [],
+    "restaurantSpecials": [{
+      "specialCategory": "",
+      "specialDays":  [],
+      "startTime": "",
+      "endTime": "",
+      "deals": [{"dealName": "",
+        "dealPrice": ""},
+        {"dealName": "",
+          "dealPrice": ""},
+        {"dealName": "",
+          "dealPrice": ""},
+        {"dealName": "",
+          "dealPrice": ""}]
+    }]
+  }
     )
   }
 
   handleChange = (event) => {
     this.setState(
-      {[event.target.name]: event.target.value}
+      { [event.target.name]: event.target.value }
     )
+    console.log(this.state)
   }
 
-  onFormSubmit = (event) => {
-    event.preventDefault()
-    this.props.ListingSubmitted(this.state)
-    this.resetForm()
+  handleChangeDeal = dealIndex => (event) => {
+    const { specials } = this.state
+    const { deals } = specials
+    let deal = deals[dealIndex] || {}
+    const changedDeal = { [event.target.name]: event.target.value }
+    console.log('changedDeal', changedDeal)
+
+    deal = Object.assign(deal, changedDeal)
+    this.setState(this.state)
   }
+
+  // onFormSubmit = (event) => {
+  //   event.preventDefault()
+  //   this.props.ListingSubmitted(this.state)
+  //   this.resetForm()
+  // }
+
+   onFormSubmit = (event) => {
+    console.log('submitted', this.state)
+    event.preventDefault()
+    const url = ''
+    const postData ={
+    "restaurantName": this.state.restaurantName,
+    "restaurantSite": this.state.restaurantSite,
+    "restaurantPhone": this.state.restaurantPhone,
+    "restaurantAddress": this.state.restaurantAddress,
+    "restaurantLocation": [],
+    "restaurantSpecials": [{
+      "specialCategory": this.state.specialCategory,
+      "specialDays":  [],
+      "startTime": this.state.startTime,
+      "endTime": this.state.endTime,
+      "deals": [{"dealName": this.state.dealName,
+        "dealPrice": this.state.dealPrice},
+        {"dealName": this.state.dealName,
+          "dealPrice": this.state.dealPrice},
+        {"dealName": this.state.dealName,
+          "dealPrice": this.state.dealPrice},
+        {"dealName": this.state.dealName,
+          "dealPrice": this.state.dealPrice}]
+    }]
+  }
+
+    console.log(postData)
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(postData),
+    })
+    .then(response => response.json())
+      this.setState(
+      {
+    "restaurantName": "",
+    "restaurantSite": "",
+    "restaurantPhone": "",
+    "restaurantAddress": "",
+    "restaurantLocation": [],
+    "restaurantSpecials": [{
+      "special_id": "",
+      "specialCategory": "",
+      "specialDays":  [false, false, false, false, false, false, false, ],
+      "startTime": "",
+      "endTime": "",
+      "deals": [{"dealName": "",
+        "dealPrice": ""},
+        {"dealName": "",
+          "dealPrice": ""},
+        {"dealName": "",
+          "dealPrice": ""},
+        {"dealName":"",
+          "dealPrice":""}]
+    }]
+  }
+    )
+   }
+
+  dateArrayFucntion() {
+    const dateArray = ['S', 'M', 'T', 'W', 'Th', 'F', 'S']
+
+  }
+
 
   render() {
+
     const format = 'h:mm a';
 
     const now = moment().hour(0).minute(0);
@@ -84,37 +161,47 @@ class AddDealForm extends React.Component {
          <Form onSubmit={this.onFormSubmit}>
              <Form.Group widths='equal'>
               <Form.Field>
-                <Form.Input placeholder='Restaurant Name' value={this.state.restaurant} onChange={this.handleChange} />
-                <Form.Input placeholder='Address' value={this.state.address} onChange={this.handleChange} />
-                <Form.Input placeholder='Phone Number' value={this.state.phone} onChange={this.handleChange}/>
-                <Form.Input placeholder='Website' value={this.state.site} onChange={this.handleChange} />
+                {}
+                <Form.Input placeholder='Restaurant Name' name="restaurantName" value={this.state.restaurantName} onChange={this.handleChange} />
+                <Form.Input placeholder='Address' name="restaurantAddress" value={this.state.restaurantAddress} onChange={this.handleChange} />
+                <Form.Input placeholder='Phone Number' name="restaurantPhone" value={this.state.restaurantPhone} onChange={this.handleChange}/>
+                <Form.Input placeholder='Website' name="restaurantSite" value={this.state.restuarantSite} onChange={this.handleChange} />
+
+                {/* <Form.Input placeholder='dealName' value={this.state.dealName} onChange={this.handleChangeDeal(3)} /> */}
               </Form.Field>
               <Form.Field>
               <label>Days of Special</label>
                 <Form.Group inline widths='equal'>
-                  <Form.Checkbox inline label='M'/>
-                  <Form.Checkbox inline label='T'/>
-                  <Form.Checkbox inline label='W'/>
-                  <Form.Checkbox inline label='Th'/>
-                  <Form.Checkbox inline label='F'/>
-                  <Form.Checkbox inline label='Sat'/>
-                  <Form.Checkbox inline label='Sun'/>
+                  <Form.Checkbox   />
+                    {Object.keys(this.state.restaurantSpecials.specialDays).map(key => <Form.Checkbox
+                        key={key}
+                    details={this.state.listings[key]}
+                    name="specialDays"
+                    value={this.state.restaurantSpecials.specialDays}
+                    onChange={this.handleChange}
+                    inline label={}
+                    />)}
                 </Form.Group>
                 <Form.Group inline widths='equal'>
-                <Form.Field label='Timeframe' control='select' value={this.state.specials[0].category} onChange={this.handleChange}>
+                <Form.Field label='Timeframe' control='select' value={this.state.restaurantSpecials[0].category} onChange={this.handleChange}>
                   <option value='Happy Hour'>Happy Hour</option>
                   <option value='lunch'>Lunch</option>
                   <option value='dinner'>Dinner</option>
                 </Form.Field>
                 </Form.Group>
-                <Form.Field placeholder='Special' control='textarea' rows='3' />
+                {/* <Form.Input placeholder='dealName' value={this.state.dealName} onChange={this.handleChangeDeal(3)} />
+                <Form.Input placeholder='dealPrice' value={this.state.dealPrice} onChange={this.handleChangeDeal(3)} />
+                <Form.Input placeholder='dealName' value={this.state.dealName} onChange={this.handleChangeDeal(3)} />
+                <Form.Input placeholder='dealPrice' value={this.state.dealName} onChange={this.handleChangeDeal(3)} />
+                <Form.Input placeholder='dealName' value={this.state.dealName} onChange={this.handleChangeDeal(3)} />
+                <Form.Input placeholder='dealPrice' value={this.state.dealPrice} onChange={this.handleChangeDeal(3)} /> */}
                 <Button type='submit'>Submit</Button>
               </Form.Field>
               </Form.Group>
-                {Object.keys(this.state.listings).map(key => <Deals
+                {/* {Object.keys(this.state.listings).map(key => <Deals
                   key={key}
                   details={this.state.listings[key]}
-              />)}
+              />)} */}
          </Form>
         </Modal>
       </div>
